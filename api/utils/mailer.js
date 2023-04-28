23c2209cbc79dd444483369c59ac300a793883b1
iLaD08 <imadeddinelakhdar08@gmail.com>
2023-04-28 20:25:01 +0100

    updaed dsd 2

--- api/utils/mailer.js
+++ api/utils/mailer.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 exports.sendEmail = async (from, to, subject, body) => {
   const { AUTH_USER, AUTH_PASS } = process.env;
   let transporter = nodemailer.createTransport({
-    host: "smtp.zoho.com",
+    host: "mail.privateemail.com",
     port: 465,
     secure: true, //ssl
     auth: {
