f4a53fca1a8a224643efe8f235a85310f82681ab
iLaD08 <imadeddinelakhdar08@gmail.com>
2023-05-10 21:40:40 +0100

    dsada

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
-// (async () => {
-//   const tunnel = await localtunnel({
-//     port: 3001,
-//     subdomain: "raaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-//   });
+(async () => {
+  const tunnel = await localtunnel({
+    port: 3001,
+    subdomain: "raaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  });
 
-//   console.log(`Running at ${tunnel.url}`);
-// })();
+  console.log(`Running at ${tunnel.url}`);
+})();
 
 server.listen(port);
